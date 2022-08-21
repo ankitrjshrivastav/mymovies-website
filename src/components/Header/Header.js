de831067ada4076e75caf71fa19c0c3ea6a72575
ankitrjshrivastav <101960479+ankitrjshrivastav@users.noreply.github.com>
2022-08-21 11:13:50 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 
 import "./Header.css";
  const Header = () => {
-  return <span onClick={() => window.scroll(0, 0)} className="header"> 🎬  My-Movies 🎬 </span>
+  return <span onClick={() => window.scroll(0, 0)} className="header"> 🎬  My-Moviess 🎬 </span>
 };
 
 export default Header;
